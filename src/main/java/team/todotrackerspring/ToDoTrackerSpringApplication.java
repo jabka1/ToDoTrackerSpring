5001e93a79b84a01b47d68c8ea9662ca1941f0dd
Artem Romaniuk <artemromaniuk4@gmail.com>
2024-12-16 20:28:57 +0200

    Saving files before merging

---
+++ src/main/java/team/todotrackerspring/ToDoTrackerSpringApplication.java
@@ -0,0 +1,13 @@
+package team.todotrackerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTrackerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoTrackerSpringApplication.class, args);
+    }
+
+}
